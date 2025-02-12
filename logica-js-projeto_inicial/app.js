c414ceedf1398924b90f5ec81d5bc2470f0897e0
Perkles <alanfilipe1996@gmail.com>
2025-02-12 20:47:41 -0300

    Second commit

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,4 +1,4 @@
-/*alert('Boas vindas ao nosso site!');
+alert('Boas vindas ao nosso site!');
 let nomee = "";
 let idadee = 25
 let numeroDeVendas = 50
@@ -6,13 +6,13 @@ let saldoDisponivel = 1000
 let mensagemDeErro = "Erro! Preencha todos os campos"
 alert(mensagemDeErro)
 let nome = prompt("Digite aqui sua idade:");
-/*let idade = prompt('Digite a sua idade');
+let idade = prompt('Digite a sua idade');
 let idade = prompt('Digite a sua idade');
 if (idade >= 18){
     alert('Pode tirar habilitação')
 }else{
     alert("Vá se lascar")
-}*/
+}
 
 let diaSemana = prompt('Qual o dia da semana?').toLowerCase(); // Converte para minúsculas
 if (diaSemana === "sábado" || diaSemana === "domingo") {
